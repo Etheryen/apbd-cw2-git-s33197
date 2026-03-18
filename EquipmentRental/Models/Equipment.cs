f54dc59b059eaffac0d82329365c6043ac8325d9
Etheryen <lukasz.ciwoniuk@gmail.com>
2026-03-18 13:08:43 +0100

    make IsAvailable settable

--- EquipmentRental/Models/Equipment.cs
+++ EquipmentRental/Models/Equipment.cs
@@ -4,6 +4,6 @@ public abstract class Equipment
 {
     public Guid Id { get; }
     public string Name { get; }
-    public bool IsAvailable { get; }
+    public bool IsAvailable { get; set; }
     public string SerialNumber { get; }
 }
